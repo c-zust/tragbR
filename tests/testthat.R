a0f93895f99d6da9731e1429dc6a9eb281ef483c
Christoph Zust <christoph.zust@statistik.ji.zh.ch>
2025-08-07 09:11:05 +0000

    fix renaming

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/articles/special-files.html
 
 library(testthat)
-library(tragbarkeitsrechner)
+library(tragbR)
 
-test_check("tragbarkeitsrechner")
+test_check("tragbR")
